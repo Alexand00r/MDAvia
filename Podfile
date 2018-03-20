d7cee54595aed7d035ed219dd869c63593076ded
Alexand00r <otpushchenko@yandex.ru>
2018-03-21 02:25:23 +0300

    Chenged first ViewController

---
+++ Podfile
@@ -0,0 +1,14 @@
+project 'tempproj-mdavia.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'tempproj-mdavia' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for tempproj-mdavia
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+
+end
--- tempproj-mdavia/FirstViewController.swift
+++ tempproj-mdavia/FirstViewController.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var TextPhoneNum: UITextField!
+    @IBOutlet weak var TextMail: UITextField!
+    
+    @IBOutlet weak var LabelPhoneNum: UILabel!
+    @IBOutlet weak var LabelMail: UILabel!
+    
+    @IBOutlet weak var TextPhoneCode: UITextField!
+    @IBOutlet weak var TextMailCode: UITextField!
 
+    
+//    @IBAction func PhoneNumEnter(_ sender: Any) {
+//        LabelPhoneNum.text = "+" + TextPhoneNum.text!
+//
+//        PhoneAuthProvider.provider().verifyPhoneNumber(LabelPhoneNum.text!, uiDelegate: nil) { (verificationID, error) in
+//            if let error = error {
+//                print(error.localizedDescription)
+//                self.showMessagePrompt(error.localizedDescription)
+//                return
+//            }
+//            // Sign in using the verificationID and the code sent to the user
+//            // ...
+//        }
+//    }
+    
+    @IBAction func MailEnter(_ sender: Any) {
+        LabelMail.text = TextMail.text!
+        
+        
+        
+    }
+    
+//    @IBAction func CheckPhoneCode(_ sender: Any) {
+//
+//        
+//    }
+    
+    @IBAction func CheckMailCode(_ sender: Any) {
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +61,9 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
 }
 
