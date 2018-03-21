b25e2dc5caa3cfa13025889229501381ae8df1a2
Alexand00r <otpushchenko@yandex.ru>
2018-03-21 23:50:03 +0300

    Storyboard
    Мелочные изменения в storyboard (с ошибками)

--- tempproj-mdavia/AppDelegate.swift
+++ tempproj-mdavia/AppDelegate.swift
@@ -21,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             FirebaseApp.configure()
             return true
     }
-
+    
+//      Исходный вариант функции
 //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 //        // Override point for customization after application launch.
 //        return true
