16d03a191fed93dd709674cca4e9b11efd36b54d
Alexand00r <otpushchenko@yandex.ru>
2018-03-21 23:00:30 +0300

    Storyboards
    Making it beauty

---
+++ tempproj-mdavia/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  tempproj-mdavia
+//
+//  Created by Alexander Otpushchenko on 21.03.2018.
+//  Copyright © 2018 Alex Otpushchenko. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
